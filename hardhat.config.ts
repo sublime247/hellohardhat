9b3e18212f81c7480ef48ee7c6ef6b9f5c3a8c6f
sublime247 <kushimobashir2000@gmail.com>
2024-08-21 08:44:42 +0100

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("@nomicfoundation/hardhat-toolbox");
+import * as dotenv from "dotenv";
+dotenv.config();
+
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";  
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
+};
+
+export default config;
